1f0a61155c621ab9e8796f36bff5936bc4409076
Sergey Polunin <poluninsa@student.bmstu.ru>
2025-10-08 01:53:14 +0300

    fixed repo

--- bot/db_repo/users.py
+++ bot/db_repo/users.py
@@ -12,11 +12,11 @@ class UsersRepo(BaseRepo):
         super().__init__(session)
 
     async def get(self, tg_user_id: int) -> Optional[User]:
-        q = select(User).where(User.tg_user_id == tg_user_id)
+        q = select(User).where(User.id == tg_user_id)
         return (await self.session.execute(q)).scalar_one_or_none()
 
     async def create(self, tg_user_id: int, tz: str = "Europe/Amsterdam") -> User:
-        user = User(tg_user_id=tg_user_id, tz=tz)
+        user = User(id=tg_user_id, tz=tz)
         await self.add(user)
         await self.session.flush()
         return user
