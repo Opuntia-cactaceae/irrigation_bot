4cd1617e4a4fcfff489d76cdadfd8325ed5a3d6c
Sergey Polunin <poluninsa@student.bmstu.ru>
2025-10-14 00:48:06 +0300

    Updated sub_cal logic

--- bot/handlers/calendar_subs_inline.py
+++ bot/handlers/calendar_subs_inline.py
@@ -149,5 +149,6 @@ async def on_calendar_subs_cb(cb: types.CallbackQuery):
 
 @calendar_subs_router.callback_query(F.data == "settings:subs_cal")
 async def cal_entry_from_settings(cb: types.CallbackQuery):
+    print("in cal_entry_from_settings")
     await show_calendar_subs_root(cb, page=1, mode="upc", action=None)
     await cb.answer()
